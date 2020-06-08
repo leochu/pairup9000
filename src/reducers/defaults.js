75acfa6d1c38e7589683ddb3baa91f315bb991bb
Leo Chu <leocchu@gmail.com>
2020-06-08 12:07:48 -0700

    update devex

--- src/reducers/defaults.js
+++ src/reducers/defaults.js
@@ -3,27 +3,36 @@
 
 export const defaultState = {
   assignments: {
-    '0': [],
-    '1': [],
+    '0': ['leo'],
+    '1': ['trevor'],
+    '2': ['josiah'],
+    '3': ['martin'],
+    '4': ['dan'],
     'out': [],
-    'pm': ['eric'],
+    'pm': ['veronica'],
     'unassigned': [
-       'leo', 'talin', 'nicholas', 'yuri',
+       'nicholas', 'sherwin',
     ],
   },
   tracks: [
-    '0', '1',
+    '0', '1', '2', '3', '4',
   ],
   trackNames: {
-    '0': 'Service Mesh',
-    '1': 'Auth',
+    '0': 'Concourse',
+    '1': 'CCPA',
+    '2': 'JS Infra',
+    '3': 'Go Infra',
+    '4': 'Python Infra',
   },
   photos: {
     'leo': 'https://avatars0.githubusercontent.com/u/4788424?s=72&v=3',
-    'eric': 'https://avatars1.githubusercontent.com/u/424145?s=72&v=3',
-    'talin': 'https://avatars0.githubusercontent.com/u/444387?s=72&v=3',
-		'nicholas': 'https://avatars2.githubusercontent.com/u/42357335?s=72&v=3',
-		'yuri': 'https://avatars2.githubusercontent.com/u/4674336?s=72&v=3',
+    'nicholas': 'https://avatars2.githubusercontent.com/u/42357335?s=72&v=3',
+    'trevor': 'https://avatars2.githubusercontent.com/u/84566?s=72&v=3',
+    'josiah': 'https://avatars2.githubusercontent.com/u/3061661?s=72&v=3',
+    'sherwin': 'https://avatars2.githubusercontent.com/u/1821241?s=72&v=3',
+    'martin': 'https://avatars2.githubusercontent.com/u/13096680?s=72&v=3',
+    'dan': 'https://avatars2.githubusercontent.com/u/48698358?s=72&v=3',
+    'veronica': 'https://avatars2.githubusercontent.com/u/52892453?s=72&v=3',
   },
   version: 0,
 }
